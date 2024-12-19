894c0f48f63d70fd37977102713d64fa68e3e7bf
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2024-12-19 03:22:03 +0200

    change URL

--- server.js
+++ server.js
@@ -97,15 +97,13 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 // الاتصال مع قاعدة البيانات MongoDB
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
 };
+
 
 // إعداد التطبيق Express
 const app = express();
